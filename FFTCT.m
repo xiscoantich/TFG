dcff94074a14054ba1e2959d3c273a804476f1a4
Xisco Antich <Xisco Antich@DESKTOP-7Q8K2I7.home>
2021-09-18 17:52:58 +0200

    Check power of 2 and vector columna

--- FFTCT.m
+++ FFTCT.m
@@ -1,6 +1,12 @@
 function [X]= FFTCT(x)
-%Separate the x[n] into even and odd-indexed subsequences
+%DIVIDE & CONQUER
+%Obtenemos un vector columna
+%Cuidado no puedes hacer las traspuesta del vector obtenido porque
+%reordena como le da la gana :')
 N=length(x);
+%Check if its power of 2
+powerof2(N);
+%Separate the x[N] into even and odd-indexed subsequences
 for r=0:(N/2-1)
     %Even
     n=2*r;
@@ -9,19 +15,16 @@ for r=0:(N/2-1)
     n=2*r+1;
     xo(r+1)=x(n+1);
 end
-if length(xe)~=length(xo)
-   cprintf('err', 'dimension error \n')
-end
 if N<=2
-    X(1)=xe+xo;
-    X(2)=xe-xo;
+    X(1,1)=xe+xo;
+    X(2,1)=xe-xo;
 else
     Xe=FFTCT(xe);
     Xo=FFTCT(xo);  
     for k=0:length(xe)-1
-        w=exp(-(2*pi*1i/N)*k);
-        X(k+1)=Xe(k+1)+w*Xo(k+1);
-        X(k+1+(N/2))=Xe(k+1)-w*Xo(k+1);
+        w=exp(-(2*pi*1i/N)*k); 
+        X(k+1,1)=Xe(k+1)+w*Xo(k+1);
+        X(k+1+(N/2),1)=Xe(k+1)-w*Xo(k+1);
     end
 end
 end
@@ -34,4 +37,9 @@ end
 %     w=e^-(2*pi*1i/N)*k;
 %     X(k)=xe
     
+function powerof2(N)
+if mod(log(N)/log(2),1)~=0
+    cprintf('err', 'NOT A POWER OF 2 \n')
+end
+end
 
