8d5b3414fd694eb374ea673e03dff1db093a0a56
Xisco Antich <xiscoantich2000@gmail.com>
2021-11-05 23:16:48 +0100

    Crear unas cuantas funciones para mejorar la visualización del codigo

--- Fourier3b1b.m
+++ Fourier3b1b.m
@@ -2,129 +2,70 @@
 clearvars 
 clc 
 close all
-% Pure signal
-Fs = 1e2;                       %Frecuencia de muestreo
-time =8;                        %Duracion de la señal
-t = 0:1/Fs:time-(1/Fs);
-f=4;                            %Frecuencia de la señal Hz
-g1 = '(sin(2*pi*f*t))';
-x = eval(g1);
-figure;
-plot(t,x);
-xlabel ('time [s]');
-ylabel ('g(t)');
-ylim([-1.5 1.5])
-xlim([0 time])
-grid on;
+
+%% Datos
+%Example 1
+Fs1 = 1e2;                     %Frecuencia de muestreo
+time1 =8;                      %Duracion de la señal
+f1=4;
+wf1 = 3.3:0.1:4.8;             %Wraping f
+Fm1=1/60;                      %freq de muestreo (para evitar trenzado Fm=1/(2*f)
+fend1=10;                      %Frecuencia mas alta para ft
+
+%Example 2
+Fs2 = 1e3;                       %Frecuencia de muestreo
+time2 =10;                       %Duracion de la señal
+f2_1=2;                            %Frecuencia de la señal Hz
+f2_2=3;                            %Frecuencia de la señal Hz
+wf2 = 1.8:0.1:3.3;
+Fm2=1/60;                      %freq de muestreo (para evitar trenzado Fm=1/(2*f)
+fend2=10;                      %Frecuencia mas alta para ft
+
+%% Calculos
+%Frecuencia de la señal Hz
+[x1,t1] = signal1(f1, Fs1, time1);
+plot_signal_1 (x1, t1);
 
 % Wraping
-figure
-%Plot wraping around the origin;
-tiledlayout(4,4);
-for wf = 3.3:0.1:4.8 %winding frequency (cycles per second)3.3:0.1:4.8
-nexttile
-g2 = 'exp(-i*2*pi*wf.*t).*(sin(2*pi*f*t))';
-x2 = eval(g2);
-plot(real(x2), imag(x2))
-ylim([-1 1])
-xlim([-1 1])
-title(['f = ',num2str(wf),'cycles/s'])
-grid on
-end
+plot_wraping (x1, wf1, t1)
 
 % Grafico f vs g(f)
-%freq de muestreo para evitar trenzado
-Fm=1/(2*f);
-%Fm=1/(4*f);
-%Calc graf
-wf_vec = 0:Fm:50;
+ft_3b1b (x1, Fs1, Fm1, fend1, t1)
 
-j=length(wf_vec);
-ft_1=zeros(1,length(wf_vec));
-for a=1:1:j
-    wf=wf_vec(a);
-    x2 = eval(g2);
-    ft_1(a)=sum(x2);
-end
-figure
-hold on
-plot(wf_vec,abs(ft_1),'b')
-grid on;
-
-%FDT red 
-xdft = fft(x);
-xdft = xdft(1:length(x)/2+1);
-freq = 0:Fs/length(x):Fs/2;
-plot(freq,abs(xdft),'r+');
-xlim([0 wf_vec(end)])
-xlabel('f [Hz]');
-
-%Recontruccion
-%Obtener y recortar la funcion
-%Aqui obtenemos la funcion pero multiplicado por 400
-g_somb=cut(ft_1,0.01);
-
-%Calculo
-%g_somb=ft_1;
-n = length(g_somb);
-wf_vec_rec=wf_vec;
-%t=linspace(0,8,n);
-j=length(t);
-for a=1:1:j
-    time_rec=t(a);
-    x1=g_somb.*exp(1i*2*pi*time_rec.*wf_vec_rec);
-    x1_rec(a)=sum(x1);
-end
-%x3_sum=ifft(g_somb);
-figure
-plot(t,real(x1_rec)/length(wf_vec))
-grid on;
+% %Recontruccion
+% %Obtener y recortar la funcion
+% %Aqui obtenemos la funcion pero multiplicado por 400
+% g_somb=cut(ft_1,0.01);
+% 
+% %Calculo
+% %g_somb=ft_1;
+% n = length(g_somb);
+% wf_vec_rec=wf_vec;
+% %t=linspace(0,8,n);
+% j=length(t);
+% for a=1:1:j
+%     time_rec=t(a);
+%     x1=g_somb.*exp(1i*2*pi*time_rec.*wf_vec_rec);
+%     x1_rec(a)=sum(x1);
+% end
+% %x3_sum=ifft(g_somb);
+% figure
+% plot(t,real(x1_rec)/length(wf_vec))
+% grid on;
 
 
 
 
 %% Chord
-clearvars;
+[x2,x2_1,x2_2,t2] = signal2(f2_1, f2_2, Fs2, time2);
+plot_signal_2 (x2,x2_1,x2_2,t2);
 
-Fs2 = 1e3;                       %Frecuencia de muestreo
-time =10;                        %Duracion de la señal
-t = 0:1/Fs2:time-(1/Fs2);
-f1=2;                            %Frecuencia de la señal Hz
-f2=3;                            %Frecuencia de la señal Hz
-g1 = '(sin(2*pi*f1*t))';
-g2 = '(sin(2*pi*f2*t))';
-x1 = eval(g1);
-x2 = eval(g2);
-xsum=x1+x2;
-figure;
-tiledlayout(3,1)
-nexttile
-plot(t,xsum);
-ylabel ('Dyad');
-grid on;
-nexttile
-plot(t,x1);
-ylabel ('g1(t)');
-grid on;
-nexttile
-plot(t,x2);
-grid on;
-xlabel ('time [s]');
-ylabel ('g2(t)');
+% Wraping
+plot_wraping (x2, wf2, t2)
 
-%% Wrapping chord
-figure
-tiledlayout(4,4);
-for wf = 1.8:0.1:3.3 %winding frequency (cycles per second)3.3:0.1:4.8
-nexttile
-g5 = '(exp(-i*2*pi*wf.*t).*(xsum))';
-x5 = eval(g5);
-plot(real(x5), imag(x5))
-ylim([-2 2])
-xlim([-2 2])
-title(['f = ',num2str(wf),'cycles/s'])
-grid on
-end
+% Grafico f vs g(f)
+ft_3b1b (x2, Fs2, Fm2, fend2, t2)
+
 
 %% Grafico f vs g(f)
 wf_vec = 0:0.005:10;
@@ -259,4 +200,83 @@ Btsort = sort(abs(Bt(:)));
 thresh = Btsort(floor((1-keep)*length(Btsort)));
 ind = abs(Bt)>thresh;       %Find small index;
 Atlow = Bt.*ind;            %Theshold small indices
+end
+
+function [g1,t1] = signal1(f, Fs, time) %(Frecuencia de la señal Hz, frequencia me muestreo, tiempo de la senyal)
+t1 = 0:1/Fs:time-(1/Fs);            
+g1 = (sin(2*pi*f*t1));
+end
+
+function [x2,x2_1,x2_2,t2] = signal2(f1, f2, Fs, time) %(Frecuencia de la señal Hz, frequencia me muestreo, tiempo de la senyal)
+t2 = 0:1/Fs:time-(1/Fs);  
+x2_1 = (sin(2*pi*f1*t2));
+x2_2 = (sin(2*pi*f2*t2));
+x2=x2_1+x2_2;
+end
+
+function plot_signal_1 (x, t)
+figure;
+plot(t,x);
+xlabel ('time [s]');
+ylabel ('g(t)');
+ylim([-1.5 1.5])
+xlim([0 t(end)])
+grid on;
+end
+
+function plot_signal_2 (x2,x2_1,x2_2,t2)
+figure;
+tiledlayout(3,1)
+nexttile
+plot(t2,x2);
+ylabel ('Dyad');
+grid on;
+nexttile
+plot(t2,x2_1);
+ylabel ('g1(t)');
+grid on;
+nexttile
+plot(t2,x2_2);
+grid on;
+xlabel ('time [s]');
+ylabel ('g2(t)');
+end
+
+function plot_wraping (x, wf, t)
+% Wraping around the origin;
+figure
+tiledlayout(2,8);
+    for i=1:1:length(wf) %winding frequency (cycles per second)3.3:0.1:4.8
+    nexttile
+    x2 = exp(-1i*2*pi*wf(i).*t).*x;
+    plot(real(x2), imag(x2))
+    ylim([-1.1*max(x) 1.1*max(x)])
+    xlim([-1.1*max(x) 1.1*max(x)])
+    title(['f = ',num2str(wf(i)),'cycles/s'])
+    grid on
+    end
+end
+
+function ft_3b1b (x, Fs, Fm, f_end, t)
+wf = 0:Fm:f_end;
+j=length(wf);
+ft_1=zeros(1,length(wf));
+for i=1:1:j
+    x2 = exp(-1i*2*pi*wf(i).*t).*x;
+    ft_1(i)=sum(x2);
+end
+figure
+hold on
+plot(wf,abs(ft_1),'b','LineWidth',2)
+grid on;
+
+%FDT red 
+xdft = fft(x);
+xdft = xdft(1:length(x)/2+1);
+freq = 0:Fs/length(x):Fs/2;
+plot(freq,abs(xdft),'r','LineWidth',1.5);
+xlim([0 f_end])
+xlabel('f [Hz]');
+legend('My code ft','Matlab fft');
+hold off;
 end
